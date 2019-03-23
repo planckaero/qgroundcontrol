06f3e67923b17f2635f5c7a767b34948cdd8bc7c
Matt Green <green@planckaero.com>
2019-03-22 17:22:24 -0700

    Added check for both newarch and legacy landing pad messages.

--- src/comm/PlanckListener.cc
+++ src/comm/PlanckListener.cc
@@ -21,9 +21,22 @@ void PlanckListener::setToolbox(QGCToolbox *toolbox)
 void PlanckListener::onMAVLinkMessage(LinkInterface* link, mavlink_message_t message)
 {
     Q_UNUSED(link);
-    if (message.msgid == MAVLINK_MSG_ID_PLANCK_LANDING_PLATFORM_STATE) {
+    if(message.msgid == MAVLINK_MSG_ID_PLANCK_LANDING_PLATFORM_STATE)
+    {
         mavlink_planck_landing_platform_state_t lps;
         mavlink_msg_planck_landing_platform_state_decode(&message, &lps);
+
+        LandingPadPosition* pos = qgcApp()->toolbox()->landingPadManager();
+
+        if(pos)
+        {
+            pos->setPosition(lps.latitude, lps.longitude);
+        }
+    }
+    else if(message.msgid == MAVLINK_MSG_ID_LANDING_PLATFORM_STATE) // For legacy compatibility
+    {
+        mavlink_landing_platform_state_t lps;
+        mavlink_msg_landing_platform_state_decode(&message, &lps);
 
         LandingPadPosition* pos = qgcApp()->toolbox()->landingPadManager();
 
