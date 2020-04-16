cb308a68473169840238b4cbaeb5377c7ba9d399
Allan Matthew <allan@planckaero.com>
2020-04-16 14:56:34 -0700

    Send LANDING_PLATFORM_STATE from local GPS

--- src/Settings/AppSettings.h
+++ src/Settings/AppSettings.h
@@ -59,6 +59,7 @@ public:
     DEFINE_SETTINGFACT(disableAllPersistence)
     DEFINE_SETTINGFACT(usePairing)
     DEFINE_SETTINGFACT(saveCsvTelemetry)
+    DEFINE_SETTINGFACT(sendPlanckGPS)
 
     // Although this is a global setting it only affects ArduPilot vehicle since PX4 automatically starts the stream from the vehicle side
     DEFINE_SETTINGFACT(apmStartMavlinkStreams)
