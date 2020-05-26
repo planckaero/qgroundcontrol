f10e5b9103fe70946bc4b300005ae669ef0ef716
Allan Matthew <allan@planckaero.com>
2020-05-26 14:05:06 -0700

    Move copiloting decoding into PlanckListener

--- src/comm/PlanckListener.cc
+++ src/comm/PlanckListener.cc
@@ -25,6 +25,25 @@ void PlanckListener::setToolbox(QGCToolbox *toolbox)
 void PlanckListener::onMAVLinkMessage(LinkInterface* link, mavlink_message_t message)
 {
     Q_UNUSED(link);
+
+    //Decode this if necessary
+    if(message.msgid == MAVLINK_MSG_ID_COPILOTING_CUSTOM)
+    {
+        mavlink_copiloting_custom_t co_msg;
+        mavlink_msg_copiloting_custom_decode(&message, &co_msg);
+
+        //Turn the payload into a mavlink message
+        if(co_msg.len <= MAVLINK_MAX_PACKET_LEN) {
+            mavlink_message_t payload_message;
+            mavlink_status_t mav_status;
+            for(int i=0; i<(int)co_msg.len; ++i) {
+                if(mavlink_parse_char(0, co_msg.data[i], &payload_message, &mav_status) == MAVLINK_FRAMING_OK) {
+                    message = payload_message;
+                }
+            }
+        }
+    }
+
     if(message.msgid == MAVLINK_MSG_ID_PLANCK_LANDING_PLATFORM_STATE)
     {
         mavlink_planck_landing_platform_state_t lps;
