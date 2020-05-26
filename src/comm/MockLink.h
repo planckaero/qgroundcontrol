061a139aeb2556594418c3487c449867d3d9ccda
Allan Matthew <allan@planckaero.com>
2020-05-26 14:05:15 -0700

    MockLink: add Planck Copiloting

--- src/comm/MockLink.h
+++ src/comm/MockLink.h
@@ -204,6 +204,7 @@ private:
     void _logDownloadWorker(void);
     void _sendADSBVehicles(void);
     void _moveADSBVehicle(void);
+    void _sendPlanckStatusCopiloting(void);
 
     static MockLink* _startMockLinkWorker(QString configName, MAV_AUTOPILOT firmwareType, MAV_TYPE vehicleType, bool sendStatusText, MockConfiguration::FailureMode_t failureMode);
     static MockLink* _startMockLink(MockConfiguration* mockConfig);
