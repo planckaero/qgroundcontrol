2cfc130b507384e7322a255dc199f5ff46c579b8
Philip Tran <phil@planckaero.com>
2020-10-15 19:23:36 -0700

    Change stop follow action and more fixes

--- src/comm/FollowTargetMonitor.cc
+++ src/comm/FollowTargetMonitor.cc
@@ -27,12 +27,18 @@ bool FollowTargetMonitor::get_target_status()
 void FollowTargetMonitor::onMAVLinkMessage(LinkInterface* link, mavlink_message_t message)
 {
     Q_UNUSED(link);
+    // Do not try to monitor target until active vehicle system ID available
+    if(active_vehicle_sysid < 0) {
+        return;
+    }
 
     if(message.msgid == MAVLINK_MSG_ID_GLOBAL_POSITION_INT && message.sysid != active_vehicle_sysid) {
+        mavlink_global_position_int_t pos;
+        mavlink_msg_global_position_int_decode(&message, &pos);
         if(!target_available) {
+            target_available = true;
             emit target_status_changed();
         }
-        target_available = true;
         target_timer.start(3000);
     }
 }
