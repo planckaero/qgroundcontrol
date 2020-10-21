81aa5f903612d684aef5c495e21e0c967566459f
Philip Tran <phil@planckaero.com>
2020-10-21 14:56:47 -0700

    Monitor target follow geocoordinates

--- src/comm/FollowTargetMonitor.cc
+++ src/comm/FollowTargetMonitor.cc
@@ -18,11 +18,15 @@ void FollowTargetMonitor::setToolbox(QGCToolbox *toolbox)
     connect(toolbox->multiVehicleManager(), &MultiVehicleManager::activeVehicleChanged, this, &FollowTargetMonitor::onActiveVehicleChanged);
 }
 
-bool FollowTargetMonitor::get_target_status()
+bool FollowTargetMonitor::get_target_health()
 {
     return target_available;
 }
 
+QGeoCoordinate FollowTargetMonitor::get_target_position()
+{
+    return target_position;
+}
 
 void FollowTargetMonitor::onMAVLinkMessage(LinkInterface* link, mavlink_message_t message)
 {
@@ -35,9 +39,17 @@ void FollowTargetMonitor::onMAVLinkMessage(LinkInterface* link, mavlink_message_
     if(message.msgid == MAVLINK_MSG_ID_GLOBAL_POSITION_INT && message.sysid != active_vehicle_sysid) {
         mavlink_global_position_int_t pos;
         mavlink_msg_global_position_int_decode(&message, &pos);
+
+        // Set target position using GLOBAL_POSITION_INT
+        target_position.setLatitude(static_cast<double>(pos.lat)/1e7);
+        target_position.setLongitude(static_cast<double>(pos.lon)/1e7);
+        target_position.setAltitude(static_cast<double>(pos.alt)/1e3);
+        emit target_position_changed(target_position);
+
+        // Emit target health changes based on flag
         if(!target_available) {
             target_available = true;
-            emit target_status_changed();
+            emit target_health_changed();
         }
         target_timer.start(3000);
     }
@@ -46,7 +58,7 @@ void FollowTargetMonitor::onMAVLinkMessage(LinkInterface* link, mavlink_message_
 void FollowTargetMonitor::onTargetTimeout()
 {
     target_available = false;
-    emit target_status_changed();
+    emit target_health_changed();
     target_timer.stop();
 }
 
