ce5e8ca24dbf9dd918c20756f75f9bf60308b394
Philip Tran <phil@planckaero.com>
2020-10-21 14:56:47 -0700

    Monitor target follow geocoordinates

--- src/comm/FollowTargetMonitor.h
+++ src/comm/FollowTargetMonitor.h
@@ -17,11 +17,19 @@ public:
     // Override from QGCTool
     virtual void setToolbox(QGCToolbox *toolbox);
 
-    Q_PROPERTY(bool target_available READ get_target_status NOTIFY target_status_changed)
+    Q_PROPERTY(bool target_available READ get_target_health NOTIFY target_health_changed)
+    Q_PROPERTY(QGeoCoordinate target_position READ get_target_position NOTIFY target_position_changed)
 
-    bool get_target_status();
+    // Get target health
+    // @return  Health of follow target
+    bool get_target_health();
+
+    // Get latest target position
+    // @return  Position of the latest follow target; this is a default constructed position prior to receiving any target
+    QGeoCoordinate get_target_position();
 signals:
-    void target_status_changed();
+    void target_health_changed();
+    void target_position_changed(QGeoCoordinate pos);
 
 public slots:
     void onMAVLinkMessage(LinkInterface* link, mavlink_message_t message);
@@ -32,6 +40,7 @@ private slots:
 
 private:
     bool target_available;
-    QTimer target_timer;
+    QTimer target_timer; //< Target health timeout
     std::atomic_int active_vehicle_sysid;
+    QGeoCoordinate target_position;
 };
