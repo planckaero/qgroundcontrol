91c466867fdf5cc18785d98d32bd27502a40f764
Allan Matthew <allan@planckaero.com>
2020-11-10 21:24:34 -0800

    Override the video stream with the value from the settings page

--- src/Camera/QGCCameraControl.cc
+++ src/Camera/QGCCameraControl.cc
@@ -1559,7 +1559,16 @@ QGCCameraControl::handleVideoInfo(const mavlink_video_stream_information_t* vi)
     _expectedCount = vi->count;
     if(!_findStream(vi->stream_id, false)) {
         qCDebug(CameraControlLog) << "Create stream handler for stream ID:" << vi->stream_id;
-        QGCVideoStreamInfo* pStream = new QGCVideoStreamInfo(this, vi);
+        QGCVideoStreamInfo* pStream;
+        if(QString(vi->uri) == "rtsp://192.168.42.1/live") {
+            mavlink_video_stream_information_t vi_modified = *vi;
+            strcpy(vi_modified.uri,qgcApp()->toolbox()->settingsManager()->videoSettings()->rtspUrl()->rawValue().toString().toStdString().c_str());
+            qDebug() << "Overriding stream address to " << QString(vi_modified.uri);
+            pStream = new QGCVideoStreamInfo(this, &vi_modified);
+        } else {
+          pStream = new QGCVideoStreamInfo(this, vi);
+        }
+
         QQmlEngine::setObjectOwnership(pStream, QQmlEngine::CppOwnership);
         _streams.append(pStream);
         //-- Thermal is handled separately and not listed
