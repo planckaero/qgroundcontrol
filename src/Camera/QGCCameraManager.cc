257c971de502344986029a2046ccf7a64551e692
Allan Matthew <allan@planckaero.com>
2020-12-10 14:25:28 -0800

    hardcoded RTSP stream

--- src/Camera/QGCCameraManager.cc
+++ src/Camera/QGCCameraManager.cc
@@ -99,7 +99,8 @@ QGCCameraManager::_mavlinkMessageReceived(const mavlink_message_t& message)
               mavlink_msg_video_stream_information_decode(&message, &vi);
               std::string uri(vi.uri);
               if(uri == "rtsp://192.168.168.10/live" || uri == "rtsp://192.168.42.1/live") {
-                  uri = qgcApp()->toolbox()->settingsManager()->videoSettings()->rtspUrl()->rawValue().toString().toStdString();
+                  uri = "rtsp://192.168.168.14:8554/detector";
+                  //uri = qgcApp()->toolbox()->settingsManager()->videoSettings()->rtspUrl()->rawValue().toString().toStdString();
                   memset(vi.uri,0,160);
                   strcpy(vi.uri,uri.c_str());
                   mavlink_message_t msg;
