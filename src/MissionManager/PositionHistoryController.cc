0aa2d58ac41759c4a72998202b9e1c38bd29f893
Allan Matthew <allan@planckaero.com>
2021-04-16 13:06:52 -0700

    Send waypoints in reverse order. 15mins max

--- src/MissionManager/PositionHistoryController.cc
+++ src/MissionManager/PositionHistoryController.cc
@@ -22,9 +22,10 @@ void PositionHistoryController::set_mission_controller(MissionController* contro
 
 void PositionHistoryController::send_mission(const QGeoCoordinate& takeoffCoord, double takeoffAlt)
 {
-  QList<QGeoPositionInfo> positions = _positionHistory->get_full_history();
+  QList<QGeoPositionInfo> positions = _positionHistory->get_history_until(15*60); //15mins
   SimpleMissionItem* item = qobject_cast<SimpleMissionItem*>(_missionController->insertTakeoffItem(takeoffCoord, -1));
   item->missionItem().setParam1(takeoffAlt);
+  std::reverse(positions.begin(), positions.end()); //reverse so we start at the most recent waypoint
   for(auto& pos : positions) {
     _missionController->insertSimpleMissionItem(pos.coordinate(), -1);
   }
