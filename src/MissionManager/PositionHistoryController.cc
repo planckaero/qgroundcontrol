c476b22030f79236f668b3f4a152628e9b2b953c
Philip Tran <phil@planckaero.com>
2021-07-16 15:22:30 -0700

    [WIP] Add survey mission item and populate polygon with position history

--- src/MissionManager/PositionHistoryController.cc
+++ src/MissionManager/PositionHistoryController.cc
@@ -1,6 +1,7 @@
 #include "PositionHistoryController.h"
 #include "QGCApplication.h"
 #include "SimpleMissionItem.h"
+#include "SurveyComplexItem.h"
 
 PositionHistoryController::PositionHistoryController(QObject* parent)
   : QObject(parent),
@@ -26,9 +27,19 @@ void PositionHistoryController::send_mission(const QGeoCoordinate& takeoffCoord,
   SimpleMissionItem* item = qobject_cast<SimpleMissionItem*>(_missionController->insertTakeoffItem(takeoffCoord, -1));
   item->missionItem().setParam1(takeoffAlt);
   std::reverse(positions.begin(), positions.end()); //reverse so we start at the most recent waypoint
-  for(auto& pos : positions) {
-    _missionController->insertSimpleMissionItem(pos.coordinate(), -1);
+
+  SurveyComplexItem* survey = qobject_cast<SurveyComplexItem*>(_missionController->insertComplexMissionItem(MissionController::patternSurveyName, takeoffCoord, -1));
+  {
+    // TODO: placeholder until polygon strategy developed (incorporate drift speed and direction)
+    QList<QGeoCoordinate> coords;
+    coords.append(takeoffCoord);
+    for(const auto& pos : positions)
+        coords.append(pos.coordinate());
+    survey->surveyAreaPolygon()->setPath(coords);
+    // TODO: change angle to point away from takeoff point
+    // 0 angle points path towards East
   }
+
   _missionController->insertLandItem(positions.back().coordinate(), -1);
   _missionController->sendToVehicle();
 }
